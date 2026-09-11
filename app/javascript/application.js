397e4cabb280d0bf49d11cbe82003d09a8514a03
Becky Seifried <36506059+rmseifried@users.noreply.github.com>
2026-09-11 12:05:47 -0400

    new css and javascript

---
+++ app/javascript/application.js
@@ -0,0 +1,6 @@
+import "@hotwired/turbo-rails";
+import "controllers";
+import * as bootstrap from "bootstrap";
+import githubAutoCompleteElement from "@github/auto-complete-element";
+import Blacklight from "blacklight";
+import Geoblacklight from "geoblacklight";
